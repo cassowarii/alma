35e3eeb5c521dcb0a420ccd8f1aeff641dc9ee37
broomweed <milesabaker@gmail.com>
2018-03-29 17:30:48 -0700

    optimize!
    secret mutability: don't allocate a new variable if one of them could be
    changed without us noticing

--- lib_op.c
+++ lib_op.c
@@ -1,5 +1,77 @@
 #include "lib.h"
 
+/* Save a memory allocation if possible when adding:
+ * if one of the variables is only referenced once,
+ * don't allocate a new one. */
+static
+AValue *sum_val(AValue *a, AValue *b) {
+    if (a->refs <= 1) {
+        a->data.i += b->data.i;
+        return ref(a);
+    } else if (b->refs <= 1) {
+        b->data.i += a->data.i;
+        return ref(b);
+    } else {
+        return ref(val_int(a->data.i + b->data.i));
+    }
+}
+
+/* Same but for subtraction. */
+static
+AValue *diff_val(AValue *a, AValue *b) {
+    if (b->refs <= 1) {
+        b->data.i -= a->data.i;
+        return ref(b);
+    } else if (a->refs <= 1) {
+        a->data.i = b->data.i - a->data.i;
+        return ref(a);
+    } else {
+        return ref(val_int(b->data.i - a->data.i));
+    }
+}
+
+/* Same but for multiplication. */
+static
+AValue *prod_val(AValue *a, AValue *b) {
+    if (a->refs <= 1) {
+        a->data.i *= b->data.i;
+        return ref(a);
+    } else if (b->refs <= 1) {
+        b->data.i *= a->data.i;
+        return ref(b);
+    } else {
+        return ref(val_int(b->data.i * a->data.i));
+    }
+}
+
+/* Same but for lessthan. */
+static
+AValue *lt_val(AValue *a, AValue *b) {
+    if (a->refs <= 1) {
+        a->data.i = b->data.i < a->data.i;
+        return ref(a);
+    } else if (b->refs <= 1) {
+        b->data.i = b->data.i < a->data.i;
+        return ref(b);
+    } else {
+        return ref(val_int(b->data.i < a->data.i));
+    }
+}
+
+/* Same but for greaterthan. */
+static
+AValue *gt_val(AValue *a, AValue *b) {
+    if (a->refs <= 1) {
+        a->data.i = b->data.i > a->data.i;
+        return ref(a);
+    } else if (b->refs <= 1) {
+        b->data.i = b->data.i > a->data.i;
+        return ref(b);
+    } else {
+        return ref(val_int(b->data.i > a->data.i));
+    }
+}
+
 /* Add the top two values on the stack. */
 void lib_add(AStack* stack, AVarBuffer *buffer) {
     AValue *a = stack_get(stack, 0);
@@ -8,7 +80,7 @@ void lib_add(AStack* stack, AVarBuffer *buffer) {
 
     // We don't do typechecking yet, so this might be garbage
     // if it's not actually an int... we'll fix this later!
-    AValue *c = ref(val_int(a->data.i + b->data.i));
+    AValue *c = sum_val(a, b);
 
     stack_push(stack, c);
     delete_ref(a);
@@ -23,7 +95,7 @@ void lib_subtract(AStack* stack, AVarBuffer *buffer) {
 
     // We don't do typechecking yet, so this might be garbage
     // if it's not actually an int... we'll fix this later!
-    AValue *c = ref(val_int(b->data.i - a->data.i));
+    AValue *c = diff_val(a, b);
 
     stack_push(stack, c);
     delete_ref(a);
@@ -38,7 +110,7 @@ void lib_multiply(AStack* stack, AVarBuffer *buffer) {
 
     // We don't do typechecking yet, so this might be garbage
     // if it's not actually an int... we'll fix this later!
-    AValue *c = ref(val_int(a->data.i * b->data.i));
+    AValue *c = prod_val(a, b);
 
     stack_push(stack, c);
     delete_ref(a);
@@ -53,7 +125,7 @@ void lib_lessthan(AStack* stack, AVarBuffer *buffer) {
 
     // We don't do typechecking yet, so this might be garbage
     // if it's not actually an int... we'll fix this later!
-    AValue *c = ref(val_int(b->data.i < a->data.i));
+    AValue *c = lt_val(a, b);
 
     stack_push(stack, c);
     delete_ref(a);
@@ -68,7 +140,7 @@ void lib_greaterthan(AStack* stack, AVarBuffer *buffer) {
 
     // We don't do typechecking yet, so this might be garbage
     // if it's not actually an int... we'll fix this later!
-    AValue *c = ref(val_int(b->data.i > a->data.i));
+    AValue *c = gt_val(a, b);
 
     stack_push(stack, c);
     delete_ref(a);
