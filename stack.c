c52508143441b3f4bf58204be29bc47f9d8a241e
broomweed <milesabaker@gmail.com>
2018-04-03 20:40:08 -0700

    don't take extra references when printing stack!

--- stack.c
+++ stack.c
@@ -59,9 +59,9 @@ void stack_pop(AStack *st, int n) {
 
 /* Print the contents of the stack. */
 void print_stack(AStack *st) {
-    for (int i = 0; i < st->size; i++) {
-        if (i != 0) printf(" ; ");
-        print_val(stack_get(st, i));
+    for (int i = st->size-1; i >= 0; i--) {
+        if (i != st->size-1) printf(" ; ");
+        print_val(st->content[i]);
     }
     printf("\n");
 }
